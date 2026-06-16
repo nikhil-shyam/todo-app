e0e65475af099df6dd1538e1c70dad73e143e1ff
nikhil-shyam <nikhildshyam@gmail.com>
2026-06-16 19:31:05 -0400

    imported a premade frontend. set up server and routing to auth and todo

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import authRoutes from './routes/authRoutes.js';
+import todoRoutes from './routes/todoRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT || 5003;
+
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url);
+// Get the driectory name from the file path
+const __dirname = dirname(__filename);
+
+// Middleware
+app.use(express.json());
+// Serves the HTML from the /public directory
+// Tells express to serve all files from the public folder as static assets/files
+// Any requests for the css files will be resolved to the public directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Serving up the HTML file from the /public directory
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/todos', todoRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server has started on port: ${PORT}`);
+});
